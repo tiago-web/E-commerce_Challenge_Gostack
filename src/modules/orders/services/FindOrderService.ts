be66f00e8629264f21f38b7e6cafae16c101dad5
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-10-25 16:44:53 -0400

    ADD CreateOrderService to create a new order

--- src/modules/orders/services/FindOrderService.ts
+++ src/modules/orders/services/FindOrderService.ts
@@ -1,7 +1,5 @@
 import { inject, injectable } from 'tsyringe';
 
-import IProductsRepository from '@modules/products/repositories/IProductsRepository';
-import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
 
